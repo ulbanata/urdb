6f192c50589f7a7eb5cb079f69277307fe7a0e9e
Patrick Brennan <chief125@gmail.com>
2014-03-10 20:06:36 -0400

    Added image link to the Actor db model

---
+++ db/migrate/20140311000553_add_image_link_to_actor.rb
@@ -0,0 +1,4 @@
+class AddImageLinkToActor < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140310233307) do
+ActiveRecord::Schema.define(version: 20140311000553) do
 
   create_table "actor_movies", force: true do |t|
     t.integer  "actor_id"
